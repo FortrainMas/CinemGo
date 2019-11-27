80b5374dea78ae32f9341e73a2a4b9375f20b53d
Ivansh927 <Ivansh92@outlook.com>
2019-11-27 22:25:40 +0700

    I completed genre choose page and add page with animation film. Now for all kids multiplication genre is animation, but may be in future I change it to for kids

---
+++ CinemaGo/Controllers/AnimationController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CinemaGo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaGo.Controllers
+{
+    public class AnimationController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (var db = new MyDbContext())
+            {
+                List<Film> films = db.Films.ToList<Film>();
+                List<Film> AnimationFilms = new List<Film>();
+                foreach (var film in films.Where(a => a.Genre == "Animation"))
+                {
+                    AnimationFilms.Add(film);
+                }
+                ViewData["Films"] = AnimationFilms;
+            }
+            return View();
+        }
+    }
+}
